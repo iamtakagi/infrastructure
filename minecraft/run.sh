f9afc34e0c18546414bb0c420e122c908636819e
iamtakagi <github@iamtakagi.net>
2024-02-24 02:15:29 +0900

    -server

--- minecraft/run.sh
+++ minecraft/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd /app/waterfall && nohup java $WATERFALL_JAVA_OPTS -jar /bin/waterfall.jar --nogui & sleep 20
-cd /app/paper && nohup java $PAPER_JAVA_OPTS -jar /bin/paper.jar --nogui
+cd /app/waterfall && java -server $WATERFALL_JAVA_OPTS -jar /bin/waterfall.jar --nogui & sleep 20
+cd /app/paper && java -server $PAPER_JAVA_OPTS -jar /bin/paper.jar --nogui
 
 # keep the container running
 tail -f /dev/null
