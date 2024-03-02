28fec582083708543878c1cf1f6af1e796ad9d84
iamtakagi <github@iamtakagi.net>
2024-03-02 11:15:12 +0900

    feat: universal

--- minecraft/init.sh
+++ minecraft/init.sh
@@ -2,14 +2,20 @@
 
 cd /build
 
-VERSION='1.20'
-BUILD='562'
+VERSION='1.16'
+BUILD='431'
 JAR_NAME=waterfall-${VERSION}-${BUILD}.jar
 URL=https://papermc.io/api/v2/projects/waterfall/versions/${VERSION}/builds/${BUILD}/downloads/${JAR_NAME}
-wget ${URL} -O waterfall.jar
+wget ${URL} -O ${JAR_NAME}
+
+VERSION='1.16.5'
+BUILD='794'
+JAR_NAME=paper-${VERSION}-${BUILD}.jar
+URL=https://papermc.io/api/v2/projects/paper/versions/${VERSION}/builds/${BUILD}/downloads/${JAR_NAME}
+wget ${URL} -O ${JAR_NAME}
 
 VERSION='1.20.4'
 BUILD='424'
 JAR_NAME=paper-${VERSION}-${BUILD}.jar
 URL=https://papermc.io/api/v2/projects/paper/versions/${VERSION}/builds/${BUILD}/downloads/${JAR_NAME}
-wget ${URL} -O paper.jar
+wget ${URL} -O ${JAR_NAME}
