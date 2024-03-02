68144277db3dc66298b7cee74bdaaeeadc5a6bd2
iamtakagi <github@iamtakagi.net>
2024-03-02 12:01:00 +0900

    Revert "feat: universal"
    This reverts commit 28fec582083708543878c1cf1f6af1e796ad9d84.

--- minecraft/run.sh
+++ minecraft/run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-cd /app/waterfall && java -server $WATERFALL_JAVA_OPTS -jar /bin/waterfall-1.16-431.jar --nogui & sleep 20
-cd /app/universal && java -server $PAPER_JAVA_OPTS -jar /bin/paper-1.16.5-794.jar --nogui & sleep 20
-cd /app/1.16.5 && java -server $PAPER_JAVA_OPTS -jar /bin/paper-1.16.5-794.jar --nogui & sleep 20
-cd /app/1.20.4 && java -server $PAPER_JAVA_OPTS -jar /bin/paper-1.20.4-424.jar --nogui
+cd /app/waterfall && java -server $WATERFALL_JAVA_OPTS -jar /bin/waterfall.jar --nogui & sleep 20
+cd /app/paper && java -server $PAPER_JAVA_OPTS -jar /bin/paper.jar --nogui
 
 # keep the container running
 tail -f /dev/null
