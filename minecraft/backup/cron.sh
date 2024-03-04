3efcf874fac5325adabf296ce719b05c9316d780
iamtakagi <github@iamtakagi.net>
2024-03-04 15:40:03 +0900

    fix: backup

--- minecraft/backup/cron.sh
+++ minecraft/backup/cron.sh
@@ -3,8 +3,6 @@
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
 mkdir -p /mnt/storage1/backup/minecraft/${DATE}
-chmod 777 -R /mnt/storage1/backup/minecraft/${DATE}
-cp -r /app/world* /mnt/storage1/backup/minecraft/${DATE}
+cp -R /app/world* /mnt/storage1/backup/minecraft/${DATE}
 mkdir -p /mnt/storage2/backup/minecraft/${DATE}
-chmod 777 -R /mnt/storage2/backup/minecraft/${DATE}
-cp -r /app/world* /mnt/storage2/backup/minecraft/${DATE}
+cp -R /app/world* /mnt/storage2/backup/minecraft/${DATE}
