9f7baf03630fcfc87894996a8021810ed91b428d
iamtakagi <github@iamtakagi.net>
2024-07-29 22:10:44 +0900

    Revert "fix"
    This reverts commit 398377c9ed83736d0996ebd0ef882adcb9c4b775.

--- minecraft/init.sh
+++ minecraft/init.sh
@@ -3,7 +3,7 @@
 cd /build
 
 VERSION='1.20.1'
-BUILD='776'
+BUILD='558'
 JAR_NAME=mohist-${VERSION}-${BUILD}-server.jar
-URL=https://mohistmc.com/api/v2/projects/mohist/${VERSION}/builds/${BUILD}/download
+URL=https://mohistmc.com/api/v2/projects/mohist/${VERSION}/builds/${BUILD}/${JAR_NAME}
 wget ${URL} -O server.jar
