fd6ed32b5bf97f817b533d9da521066d800d84cb
iamtakagi <github@iamtakagi.net>
2024-07-31 17:19:23 +0900

    fix: dl path

--- minecraft/init.sh
+++ minecraft/init.sh
@@ -3,7 +3,7 @@
 cd /build
 
 VERSION='1.20.1'
-BUILD='558'
+BUILD='776'
 JAR_NAME=mohist-${VERSION}-${BUILD}-server.jar
-URL=https://mohistmc.com/api/v2/projects/mohist/${VERSION}/builds/${BUILD}/${JAR_NAME}
+URL=https://mohistmc.com/api/v2/projects/mohist/${VERSION}/builds/${BUILD}/download
 wget ${URL} -O server.jar
