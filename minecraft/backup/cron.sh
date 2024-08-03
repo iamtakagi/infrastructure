ca44cad26b44271e9e59b763674afba6e552f25b
iamtakagi <github@iamtakagi.net>
2024-08-03 17:44:30 +0900

    いろいろ

--- minecraft/backup/cron.sh
+++ minecraft/backup/cron.sh
@@ -2,7 +2,29 @@
 
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
+discord_url="https://discord.com/api/webhooks/1269212346677723157/gOCRwxryLQ-tgqa3BfTnn42IwNMQwpta2t1-oUUCuf45gmPHtpEtgX9Z5R2KElSYYAcj"
+
+send_discord_notification() {
+  local message=$1
+  
+  # Construct payload
+  local payload=$(cat <<EOF
+{
+  "content": "$message"
+}
+EOF
+)
+
+  # Send POST request to Discord Webhook
+  curl -H "Content-Type: application/json" -X POST -d "$payload" $discord_url
+}
+
 mkdir -p /mnt/storage1/backup/minecraft/${DATE}
 cp -R /app/world* /mnt/storage1/backup/minecraft/${DATE}
+
+send_discord_notification "ワールドデータ (world, world_nether, world_the_end) のバックアップ: /mnt/storage1/backup/minecraft/${DATE} (プライマリストレージ) に保存されました。"
+
 mkdir -p /mnt/storage2/backup/minecraft/${DATE}
 cp -R /app/world* /mnt/storage2/backup/minecraft/${DATE}
+
+send_discord_notification "ワールドデータ (world, world_nether, world_the_end) のバックアップ: /mnt/storage2/backup/minecraft/${DATE} (セカンダリストレージ) に保存されました。"
