456fe2efa2260065f52833340ede1e35f917a518
iamtakagi <github@iamtakagi.net>
2024-08-30 23:05:04 +0900

    rm

--- minecraft/db/init/create-database-coreprotect.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER coreprotect IDENTIFIED BY 'coreprotect';
-CREATE DATABASE `coreprotect`;
-GRANT ALL PRIVILEGES ON `coreprotect`.* TO coreprotect IDENTIFIED BY 'coreprotect';
--- minecraft/velocity/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /app/velocity && java -server $JAVA_OPTS -jar /bin/velocity.jar
-
-tail -f /dev/null
